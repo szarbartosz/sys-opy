789eaf1760582cbcfa4d2cd1b2000cadf7632810
szarbartosz <szarbartosz@gmail.xom>
2020-03-16 18:30:36 +0100

    ptsd confirmed

---
+++ zestaw1/zad3b/blocks.h
@@ -0,0 +1,18 @@
+struct Table{
+    int length;
+    int *editOpsBlocksLength;
+    char ***editOpsBlocks;
+};
+
+char *concat(const char *str1, const char *str2);
+void compareFiles(char *file1, char *file2);
+void compareSequence(char *sequence);
+int countEditingOperations();
+char **createEditingOperationsBlock();
+struct Table initTable();
+int addBlockOfEditingOperations(struct Table *mainTab);
+void deleteOperation(struct Table *mainTab, int blockIndex, int operationIndex);
+void deleteBlock(struct Table *mainTable, int blockIndex);
+void deleteTable(struct Table *mainTab);
+
+
